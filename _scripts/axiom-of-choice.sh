56432e84a1a633ea1cc11388b2aed9735643fc54
Zhijie Chen <zhijie.chen2007@outlook.com>
2025-11-11 01:33:39 +0800

    Modify scripts

--- _scripts/axiom-of-choice.sh
+++ _scripts/axiom-of-choice.sh
@@ -1,2 +1,2 @@
-cp ~/axiom-of-choice-blog/*.tex ./assets/img/posts/axiom-of-choice/
-cp ~/axiom-of-choice-blog/*.pdf ./assets/img/posts/axiom-of-choice/
+cp ~/blog-posts-latex/axiom-of-choice-blog/*.tex ./assets/img/posts/axiom-of-choice/
+cp ~/blog-posts-latex/axiom-of-choice-blog/*.pdf ./assets/img/posts/axiom-of-choice/
---
+++ _scripts/kl-divergence.sh
@@ -0,0 +1,2 @@
+cp ~/blog-posts-latex/kl-divergence-blog/*.tex ./assets/img/posts/kl-divergence/
+cp ~/blog-posts-latex/kl-divergence-blog/*.pdf ./assets/img/posts/kl-divergence/
